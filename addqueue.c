b73c64a22f80a2e65f31ccbec317de3885613299
lia237 <https://github.com/lia237/alx-low_level_programming.git>
2023-08-27 16:35:18 -0700

    test

--- addqueue.c
+++ addqueue.c
@@ -1,16 +1,4 @@
 #include "monty.h"
-/**
- * f_queue - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_queue(stack_t **head, unsigned int counter)
-{
-	(void)head;
-	(void)counter;
-	bus.lifi = 1;
-}
 
 /**
  * addqueue - add node to the tail stack
@@ -46,3 +34,4 @@ void addqueue(stack_t **head, int n)
 		new_node->prev = aux;
 	}
 }
+
